ff41fc874e7083391f925929922943605bf8f33f
LojanoE <estanislaolojano@gmail.com>
2026-07-28 08:55:46 -0500

    SECRETS

--- apps/web/src/firebase/config.ts
+++ apps/web/src/firebase/config.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || 'AIzaSyAsr3a6N0JM_NdYhN4YbOSKH1zTUAVSDI0',
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'manejo-centro-aa.firebaseapp.com',
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || 'manejo-centro-aa',
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || 'manejo-centro-aa.firebasestorage.app',
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '56404747919',
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || '1:56404747919:web:f0e8f3cc416cdbd946b2fa',
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || 'G-GY9T56MZ32',
 }
 
 if (!firebaseConfig.apiKey || !firebaseConfig.projectId) {
